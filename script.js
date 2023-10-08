842f917bfd40528df49fa7a5a1c04576c4a8911c
DevanCapps <145359970+devancapps@users.noreply.github.com>
2023-10-07 23:51:01 -0500

    Wrote code for saving user info to local storage

--- script.js
+++ script.js
@@ -5,11 +5,12 @@ $(function () {
       var userInput = $(this).siblings(".description").val();
       
       // Get the hour from the id of the parent time-block
-     } var hour = $(this).parent().attr("id");
+      var hour = $(this).parent().attr("id");
   
       // Save user input in local storage using the hour as the key
       localStorage.setItem(hour, userInput);
     });
+
      // Get the current hour using Day.js
   var currentHour = dayjs().format("H");
 
@@ -25,3 +26,25 @@ $(function () {
     } else {
       $(this).addClass("future");
     }
+
+        // Get the hour from the id of the time-block
+    var hour = $(this).attr("id");
+
+    // Retrieve user input from local storage
+    var savedInput = localStorage.getItem(hour);
+
+    // Check if there's saved input for this hour
+    if (savedInput !== null) {
+      // Set the textarea value with the saved input
+      $(this).find(".description").val(savedInput);
+    }
+  });
+
+  // Get the current date using Day.js and format it
+  var currentDate = dayjs().format("dddd, MMMM D, YYYY");
+
+  // Set the text of the currentDay element
+  $("#currentDay").text(currentDate);
+});
+
+    
