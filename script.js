bbede13ee623ba0f183fa8366dd4c71a43e092d8
DevanCapps <145359970+devancapps@users.noreply.github.com>
2023-10-07 23:32:00 -0500

    Built loop for time block

--- script.js
+++ script.js
@@ -10,3 +10,18 @@ $(function () {
       // Save user input in local storage using the hour as the key
       localStorage.setItem(hour, userInput);
     });
+     // Get the current hour using Day.js
+  var currentHour = dayjs().format("H");
+
+  // Loop through each time block
+  $(".time-block").each(function () {
+    var blockHour = parseInt($(this).attr("id").split("-")[1]);
+
+    // Compare the block's hour with the current hour
+    if (blockHour < currentHour) {
+      $(this).addClass("past");
+    } else if (blockHour === currentHour) {
+      $(this).addClass("present");
+    } else {
+      $(this).addClass("future");
+    }
